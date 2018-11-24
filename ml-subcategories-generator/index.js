5277a6519a2f35aa581c1c8b539cb23e737bd91b
Alexandra Bodirlau <alexandra.bodirlau@gmail.com>
2018-11-24 18:37:00 +0200

    Fixed indentation

--- ml-subcategories-generator/index.js
+++ ml-subcategories-generator/index.js
@@ -2,25 +2,25 @@
 const CONSTANTS = require('./constants.json');
 const language = require('@google-cloud/language');
 const client = new language.LanguageServiceClient({
-		projectId: 'roundtrip-1543001890834',
-		keyFilename: 'C:\\Users\\Alex\\Desktop\\2NHack\\nlp.json'
-	});
+  projectId: 'roundtrip-1543001890834',
+  keyFilename: 'C:\\Users\\Alex\\Desktop\\2NHack\\nlp.json'
+});
 
 
 module.exports = {
-	get_subcategories,
+  get_subcategories,
 }
 
 
 function get_subcategories(text, callback) {
-	console.log("Predicting categories...");
-	
-    const document = {
-  		content: text,
-  		type: 'PLAIN_TEXT',
-	};
+  console.log("Predicting categories...");
+  
+  const document = {
+    content: text,
+    type: 'PLAIN_TEXT',
+  };
     
-	client
+  client
   .classifyText({document: document})
   .then(results => {
     var classification = results[0];
@@ -28,36 +28,36 @@ function get_subcategories(text, callback) {
     process_categories(classification.categories, callback);
   })
   .catch(err => {
-  	callback(err);
+    callback(err);
   });
 }
 
 function process_categories(categories, callback) {
-	var map = new Map();
-    var subcat;
-    var counter = categories.length;
+  var map = new Map();
+  var subcat;
+  var counter = categories.length;
 
-    categories.forEach(raw_subcat => {
-    	subcat = CONSTANTS.raw_subcategories[raw_subcat.name];
+  categories.forEach(raw_subcat => {
+    subcat = CONSTANTS.raw_subcategories[raw_subcat.name];
 
-    	if (subcat != undefined) {
+    if (subcat != undefined) {
 
-    		subcat.forEach(elem => {
-    			if (map.has(elem)) {
-    				map.set(elem, Math.max(raw_subcat.confidence, map.get(elem)));
+      subcat.forEach(elem => {
+        if (map.has(elem)) {
+          map.set(elem, Math.max(raw_subcat.confidence, map.get(elem)));
 
-    			} else {
-    				map.set(elem, raw_subcat.confidence);
-    			}
-    		});
-    	}
+        } else {
+          map.set(elem, raw_subcat.confidence);
+        }
+      });
+    }
 
-    	if (--counter == 0) {
-    		result = Array.from(map, el => {
-    			return {name: el[0], confidence: el[1]}
-    		});
+    if (--counter == 0) {
+      result = Array.from(map, el => {
+        return {name: el[0], confidence: el[1]}
+      });
 
-			callback(null, result);    		
-    	}
-    });
+    callback(null, result);       
+    }
+  });
 }
